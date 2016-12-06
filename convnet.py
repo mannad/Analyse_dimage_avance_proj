f63c8923886889b38b94eca89972b5b37f64c68b
Carl Lemaire <c.lemaire@live.ca>
2016-12-06 15:52:06 -0500

    Extract preprocessing

--- convnet.py
+++ convnet.py
@@ -4,17 +4,34 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
-from keras import backend as kback
 
 np.random.seed(1337)  # for reproducibility
-# kback.set_image_dim_ordering('tf')
+
+
+def preprocess_mnist():
+    # the data, shuffled and split between train and test sets
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    X_train = X_train.reshape(X_train.shape + (1,))
+    X_test = X_test.reshape(X_test.shape + (1,))
+    input_shape = (X_train.shape[1], X_train.shape[2], 1)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+    return (X_train, Y_train), (X_test, Y_test), input_shape
+
 
 batch_size = 128  # TODO hyperparameter for gridsearch?
 nb_classes = 10
 nb_epoch = 4
 
-# input image dimensions
-img_rows, img_cols = 28, 28
 # number of convolutional filters to use
 nb_filters = 8  # TODO hyperparameter for gridsearch?
 # size of pooling area for max pooling
@@ -22,22 +39,10 @@ pool_size = (2, 2)
 # convolution kernel size
 kernel_size = (3, 3)
 
-# the data, shuffled and split between train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# Preprocess dataset
+(X_train, Y_train), (X_test, Y_test), input_shape = preprocess_mnist()
 
-X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-input_shape = (img_rows, img_cols, 1)
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
-
+# Build convolution network
 model = Sequential()
 
 model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],

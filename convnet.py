4f09a429ffdb69dc2ab103208680de4511cd67f7
Carl Lemaire <c.lemaire@live.ca>
2016-12-06 16:14:03 -0500

    Extract network contruction

--- convnet.py
+++ convnet.py
@@ -6,6 +6,7 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 
 np.random.seed(1337)  # for reproducibility
+NB_CLASSES = 10
 
 
 def preprocess_mnist():
@@ -22,8 +23,8 @@ def preprocess_mnist():
     X_test /= 255
 
     # convert class vectors to binary class matrices
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+    Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 
     return (X_train, Y_train), (X_test, Y_test), input_shape
 
@@ -40,15 +41,41 @@ def preprocess_cifar10():
     X_test /= 255
 
     # convert class vectors to binary class matrices
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+    Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 
     return (X_train, Y_train), (X_test, Y_test), input_shape
 
 
+def build_network(nb_filters, kernel_size, input_shape, pool_size):
+    # Build convolution network
+    model = Sequential()
+
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                            border_mode='valid',
+                            input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NB_CLASSES))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+
+    return model
+
+
 batch_size = 128  # TODO hyperparameter for gridsearch?
-nb_classes = 10
-nb_epoch = 4
+nb_epoch = 6
 
 # number of convolutional filters to use
 nb_filters = 8  # TODO hyperparameter for gridsearch?
@@ -60,31 +87,14 @@ kernel_size = (3, 3)
 # Preprocess dataset
 (X_train, Y_train), (X_test, Y_test), input_shape = preprocess_mnist()
 
-# Build convolution network
-model = Sequential()
+# Build network
+model = build_network(nb_filters, kernel_size, input_shape, pool_size)
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                        border_mode='valid',
-                        input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
-
+# Train
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
           verbose=1, validation_split=1/7)
+
+# Test
 score = model.evaluate(X_test, Y_test, verbose=0)
 print(model.metrics_names[0], score[0])
 print(model.metrics_names[1], score[1])

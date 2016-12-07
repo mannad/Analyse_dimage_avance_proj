e569748ab096fcea20dcde4aca9ee3980312c81e
Carl Lemaire <c.lemaire@live.ca>
2016-12-07 13:09:34 -0500

    Add layers to have 4

--- convnet.py
+++ convnet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from keras.datasets import mnist, cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -51,9 +52,14 @@ def build_network(nb_filters, kernel_size, input_shape, pool_size):
     # Build convolution network
     model = Sequential()
 
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                            border_mode='valid',
-                            input_shape=input_shape))
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], border_mode='valid', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.1))
+
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
     model.add(Activation('relu'))
     model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
     model.add(Activation('relu'))
@@ -73,43 +79,44 @@ def build_network(nb_filters, kernel_size, input_shape, pool_size):
 
     return model
 
-
-batch_size = 128  # TODO hyperparameter for gridsearch?
 nb_epoch = 6
-
-# number of convolutional filters to use
-nb_filters = 8  # TODO hyperparameter for gridsearch?
+pool_size = (2, 2)
+kernel_size = (3, 3)
 
 # Preprocess dataset
 (X_train, Y_train), (X_test, Y_test), input_shape = preprocess_mnist()
 
 # Grid search
-best_accu = 0
-best_params = None
-
-list_nb_filters = [4, 8, 16, 32, 64]
-list_batch_size = [64, 128, 256, 512, 1024]
+list_nb_filters = [4, 8, 16, 32, 64, 128, 256]
+list_batch_size = [64, 128, 256, 512, 1024, 2048]
 
 grid_search_results = np.zeros((len(list_nb_filters), len(list_batch_size)))
 
+with open("cumulative_results.txt", "a") as file:
+    file.write("\n\nNew grid search ==== " + time.ctime() + "\n")
+    file.write("Nb. epochs: {}   Kernel size: {}   Pool size: {}\n".format(nb_epoch, kernel_size, pool_size))
+
 for i, nb_filters in enumerate(list_nb_filters):
     # Build network
-    model = build_network(nb_filters, kernel_size=(3, 3), input_shape=input_shape, pool_size=(2, 2))
+    model = build_network(nb_filters, kernel_size=kernel_size, input_shape=input_shape, pool_size=pool_size)
 
     for j, batch_size in enumerate(list_batch_size):
+        if i < 4 and j < 5:
+            continue
+
         # Train
         model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
                   verbose=1, validation_split=1/7)
 
         # Test
         loss, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+        print(nb_filters, batch_size)
         print(model.metrics_names[0], loss)
         print(model.metrics_names[1], accuracy)
 
         grid_search_results[i, j] = accuracy
 
-        if accuracy > best_accu:
-            best_accu = accuracy
-            best_params = (nb_filters, batch_size)
+        with open("cumulative_results.txt", "a") as file:
+            file.write("{:<3} {:<5} {:<.3f} {:<.3f}\n".format(nb_filters, batch_size, loss, accuracy))
 
 np.savetxt("gridsearch.csv", grid_search_results, delimiter="\t")
